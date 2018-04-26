519bb670afb18d2739758fa30058eaf74ac76653
robby1212 <sarvis.server1@gmail.com>
2018-04-25 21:34:47 -0500

    Add files via upload
    NOT DONE

--- mycat.c
+++ mycat.c
@@ -6,58 +6,11 @@
 #include <sys/stat.h>
 #include <stdbool.h>
 
-void mycat ( char *fileName ) {
+void mycat ( char *fileToOpen, char *outputFile, int argc ) {
 
-    char outputFile[500];
-    char fileToOpen[500];
     int outputFlag = 1;
-    int inputFlag = 1;
-    int exitFlag = 1;
- 
-    int i = 0;
-    int count = 0;
+    
 
-    char *pos;
-    if ((pos=strchr(fileName, '\n')) != NULL)
-        *pos = '\0';
-
-    while ( (fileName[i] != '\0') && (exitFlag == 1) && (fileName[i] != '\n') 
-            && (fileName[i] != EOF) )
-    { 
-        if ( fileName[i] == '>' )
-        {
-            outputFlag = 0;
-            int outCount = 0;
-            for ( int n = i+1; (fileName[n] != '\0') 
-                    && (fileName[n] != ' '); ++n )
-            {
-                outputFile[outCount] = fileName[n];
-                ++outCount;
-            }
-            exitFlag = 0;
-        }
-        else if ( fileName[i] == '<' )
-        {
-            inputFlag = 0;
-            int inCount = 0;
-            for ( int j = i+1; (fileName[j] != '\0') && (fileName[j] != '>') 
-                && (fileName[j] != ' '); ++j )
-            {
-                fileToOpen[inCount] = fileName[j];
-                ++inCount;
-            }
-        }
-        else
-        {
-            if ( (fileName[i] != ' ') && (inputFlag==1) && (fileName[i] != '\0') )
-            {
-            fileToOpen[count] = fileName[i];
-            ++count; 
-            }
-        }
-    ++i;
-    }
-printf("%s", fileToOpen);
     if ( (access ( fileToOpen, F_OK ) != -1) && (outputFlag == 1) )
     {
         FILE *fptr = fopen(fileToOpen, "r");
@@ -98,15 +51,17 @@ printf("%s", fileToOpen);
     }
     else 
     {
-        printf("ERROR mycat: %s: No such file or directory\n", fileName);
+        printf("ERROR mycat: %s: No such file or directory\n", fileToOpen);
     }
 }
 
-int main () {
-    char fileName[500];
-    printf("Input File to CAT: ");
-    fgets(fileName, sizeof(fileName), stdin);
+int main (int argc, char** argv) {
+    if (argc <= 1)
+    {
+        printf("No file specified");
+        exit(1);
+    }
 
-    mycat(fileName);
+    mycat(argv[1], argv[2], argc);
     return 0;
 }
