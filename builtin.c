d8b16406ca24ed35e3ae8e348e02ec1b3848d591
James Bellian <32314548+jtwbellian@users.noreply.github.com>
2018-04-25 20:54:50 -0500

    Added the builtin functions
    cd and pwd for the shell

---
+++ builtin.c
@@ -0,0 +1,36 @@
+// mycd - changes the current directory
+// pwd - prints the working directory 
+// Programmed by James Bellian
+
+#include<stdio.h>
+#include<string.h>
+#define MAXSIZE 500
+
+int mycd(char * comm[])
+{
+	if (comm[1] == NULL)
+	{
+		printf("Expected argument - none given!\n");
+		return 0;
+	} 
+
+	char dir[MAXSIZE];
+	strcpy(dir, comm[1]);
+	
+	if (chdir(comm[1]) < 0) 
+	{
+		printf("Unknown directory \"%s\"\n", dir);
+		return 0;
+	}
+	
+	return 1;
+
+}
+
+int pwd()
+{
+	char wd[MAXSIZE];
+	getcwd(wd,MAXSIZE);	
+	printf("%s\n", wd);	
+}
+
